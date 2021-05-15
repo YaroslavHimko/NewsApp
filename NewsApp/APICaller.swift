4316828f370735f6414a970a99ad6988f72999b7
Yaroslav Himko <yaroslav.himko@gmail.com>
2021-05-16 02:01:50 +0300

    Implemented API calls and layout

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,59 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by Yaroslav Himko on 16.05.21.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/everything?q=tesla&from=2021-04-15&sortBy=publishedAt&apiKey=ad15902a63f949378554228125324151")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping(Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
